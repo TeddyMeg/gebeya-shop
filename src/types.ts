0b78ebdccef262fad201b6633c2c0f7930470bce
Tewodros Tassew <teddy.meg1234@gmail.com>
2024-11-24 14:13:06 +0800

    Second commit - Created types.ts file and added the product and cart item types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    description: string;
+    category: string;
+  }
+  
+  export interface CartItem extends Product {
+    quantity: number;
+  }
+  
+  export interface CheckoutForm {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    address: string;
+    city: string;
+    zipCode: string;
+  }
